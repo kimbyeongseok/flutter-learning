6caea9bc543200739ceaa27630766538b74c712f
kimbyeongseok <69345685+kimbyeongseok@users.noreply.github.com>
2020-09-24 14:23:53 +0900

    scroll view function

--- main.dart
+++ main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
@@ -8,38 +8,90 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Bottom nav bar',
+      title: 'Scroll View',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: MyHome(),
+      home: HomePage(),
     );
   }
 }
-class MyHome extends StatelessWidget {
+//*===========single child scroll view============*
+//class HomePage extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    final items = List.generate(100, (i) => i).toList();
+//    //https://api.flutter.dev/flutter/dart-core/List/List.generate.html
+//    return Scaffold(
+//      appBar: AppBar(
+//        title: Text('Single Child Scroll View'),
+//      ),
+//      body: SingleChildScrollView(
+//        child: ListBody(
+//          children: items.map((i)=>Text('$i')).toList(),
+//        ),
+//      ),
+//    );
+//  }
+//}
+//*==================List View=====================*
+//class HomePage extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      appBar: AppBar(
+//        title: Text('List View'),
+//      ),
+//      body:ListView(
+//        children: [
+//          ListTile(
+//            leading: Icon(Icons.home),
+//            title: Text('Home'),
+//            trailing: Icon(Icons.navigate_next),
+//            onTap: (){},
+//          ),
+//          ListTile(
+//            leading: Icon(Icons.event),
+//            title: Text('Event'),
+//            trailing: Icon(Icons.navigate_next),
+//            onTap: (){},
+//          ),
+//          ListTile(
+//            leading: Icon(Icons.camera),
+//            title: Text('Camera'),
+//            trailing: Icon(Icons.navigate_next),
+//            onTap: (){},
+//          ),
+//        ],
+//      ),
+//    );
+//  }
+//}
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Bottom nav bar'),
-      ),
-      //*=======2 or over navigation buttons under app display=====*
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text('Home'),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text('Tap Bar View')),
+          bottom: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.tag_faces)),
+              Tab(text:'Menu2'),
+              Tab(icon: Icon(Icons.info)),
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            title: Text('profile'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.notifications),
-            title: Text('Alarm'),
-          ),
-        ],
+        ),
+        body:TabBarView(
+          children: [
+            Container(color: Colors.red),
+            Container(color: Colors.yellow),
+            Container(color: Colors.blue),
+          ],
+        ),
       ),
     );
+
   }
 }
