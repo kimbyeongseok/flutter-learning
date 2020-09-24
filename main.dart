8b2bbfc0d0809f69b11bb4d68025417b63286b0a
kimbyeongseok <69345685+kimbyeongseok@users.noreply.github.com>
2020-09-24 14:25:01 +0900

    basic ui layout

--- main.dart
+++ main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
@@ -8,38 +8,179 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Bottom nav bar',
+      title: 'Basic UI',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey
       ),
-      home: MyHome(),
+      home: BasicUI(),
     );
   }
 }
-class MyHome extends StatelessWidget {
+class BasicUI extends StatefulWidget {
+  @override
+  _BasicUIState createState() => _BasicUIState();
+}
+
+class _BasicUIState extends State<BasicUI> {
+
+  var _index = 0;
+  var _pages = [
+    Page1(),
+    Page2(),
+    Page3(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Bottom nav bar'),
+        backgroundColor: Colors.white,
+        title: Text(
+          'Basic_UI',
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,//텍스트 중앙 위치
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.add,
+              color: Colors.black,
+            ),
+            onPressed: (){
+            },
+          )
+        ],
       ),
-      //*=======2 or over navigation buttons under app display=====*
+      body: _pages[_index],//변수 _pages 배열 안에 _index 번째 페이지 호출
       bottomNavigationBar: BottomNavigationBar(
+        onTap: (index){
+          setState(() {
+            _index = index;//탭이 이루어진 인덱스로 _index 변수를 변
+          });
+        },
+        currentIndex: _index,
         items: [
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
             title: Text('Home'),
+            icon: Icon(Icons.home),
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            title: Text('profile'),
+            title: Text('User Services'),
+            icon: Icon(Icons.assignment),
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.notifications),
-            title: Text('Alarm'),
+            title: Text('Info'),
+            icon: Icon(Icons.account_circle),
           ),
         ],
       ),
     );
   }
 }
+class Page1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _buildTop(),
+        _buildMiddle(),
+        _buildBottom(),
+      ],
+    );
+  }
+  Widget _buildTop(){
+    return Padding(
+      padding: const EdgeInsets.only(top: 20, bottom: 20),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  Icon(Icons.local_taxi, size: 40,),
+                  Text('Taxi'),
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(Icons.local_taxi, size: 40,),
+                  Text('Taxi'),
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(Icons.local_taxi, size: 40,),
+                  Text('Taxi'),
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(Icons.local_taxi, size: 40,),
+                  Text('Taxi'),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(height: 20.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  Icon(Icons.local_taxi, size: 40,),
+                  Text('Taxi'),
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(Icons.local_taxi, size: 40,),
+                  Text('Taxi'),
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(Icons.local_taxi, size: 40,),
+                  Text('Taxi'),
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(Icons.local_taxi, size: 40,),
+                  Text('Taxi'),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+  Widget _buildMiddle(){
+    return Text('Middle');
+  }
+  Widget _buildBottom(){
+    return Text('Bottom');
+  }
+}
+class Page2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'User Services',
+        style: TextStyle(fontSize: 40),),
+    );
+  }
+}
+class Page3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Information',
+        style: TextStyle(fontSize: 40),),
+    );
+  }
+}
+
